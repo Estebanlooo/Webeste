d6e9c3e1531ea846d1d2b6a30813b03296c5058d
Estebanlooo <93783258+Estebanlooo@users.noreply.github.com>
2022-01-06 14:58:14 +0100

    8
    suii

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 function test1()
 {
-    var a = document.getElementById('val1').value
-    let b = 'Stranger Things'
-    if (a = b)
+    var a = document.getElementById('val1').value 
+    if (a == "Stranger Things")
         alert('Bonne réponse.')
     else alert('Réponse fausse.')
+}
 
-
+function choix()
+{
+    if (document.qcm1.choix[2].checked) 
+        alert('Bonne réponse.') 
+    else alert('Réponse fausse.')
 }
